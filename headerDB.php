d43f01f61ff9cff81e765508a16007a0255e7cb6
Raphael Petrozzi <86420345+donpoulp@users.noreply.github.com>
2021-08-08 12:09:44 +0200

    ajout de la db heroku

--- headerDB.php
+++ headerDB.php
@@ -1,4 +1,4 @@
 <?php
 // initialisation de ma base de donnees sur un heaer pour l'ouvrire sur mes diferrente page
-$bdd = new PDO('mysql:host=localhost:3307;dbname=livredor', 'raphael', 'petrozzi');
+$bdd = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com:3306;dbname=livredor', 'b38c8e36fafdd8', '4efd58f0');
 ?>
