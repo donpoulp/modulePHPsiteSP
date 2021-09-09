32f656deffeebef7c6d1cc09e3077ca7505a84c3
donpoulp <raphael.petrozzi@le-campus-numerique.fr>
2021-09-09 09:55:31 +0200

    headerDB

--- headerDB.php
+++ headerDB.php
@@ -1,5 +1,5 @@
 <?php
 // initialisation de ma base de donnees sur un heaer pour l'ouvrire sur mes diferrente page
-$bdd = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com:3306;dbname=heroku_e5d194c81183d75', 'b38c8e36fafdd8', '4efd58f0');
-// $bdd = new PDO('mysql:host=localhost:3306;dbname=spacetrip', 'raphael', 'petrozzi');
+// $bdd = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com:3306;dbname=heroku_e5d194c81183d75', 'b38c8e36fafdd8', '4efd58f0');
+$bdd = new PDO('mysql:host=localhost:3306;dbname=spacetrip', 'raphael', 'petrozzi');
 ?>
