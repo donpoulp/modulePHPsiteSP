21226c963011a616c18689dadcd4a267d9d03ced
Virginie-Villard <virginie.villard@le-campus-numerique.fr>
2021-09-09 15:05:32 +0200

    commit aleatoire

--- headerDB.php
+++ headerDB.php
@@ -3,6 +3,6 @@
 include 'settings.php';
 // initialisation de ma base de donnees sur un heaer pour l'ouvrire sur mes diferrente page
 // $bdd = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com:3306;dbname=heroku_e5d194c81183d75', 'b38c8e36fafdd8', '4efd58f0');
-$bdd = new PDO('mysql:host=localhost:'.$post.';dbname=spacetrip', $user, $pass);
+$bdd = new PDO('mysql:host=localhost:3306;dbname=spacetrip', $user, $pass);
 
 ?>
